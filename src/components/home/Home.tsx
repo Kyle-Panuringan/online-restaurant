9f5c62b5b28a26b4d37e59ed388519f3b0ff4bb0
Kyle Panuringan <kylepanuringan@gmail.com>
2022-08-23 20:00:25 +0800

    Add simple arrow indicator animation

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,18 @@
 import "./home.scss";
-
 const Home = () => {
-	return <div className="home">Home</div>;
+	return (
+		<div className="home">
+			<h2>
+				<i>"Bon Appétit"</i>
+				<br />
+				<i>At</i>
+				<br />
+				<i>Online Restaurant</i>
+			</h2>
+			<button>View Menu</button>
+			<span id="arrow-indicator">︽</span>
+		</div>
+	);
 };
 
 export default Home;
