f477ca172e2dd239d07c1b6d386c85e0fa17e4f6
Kyle Panuringan <kylepanuringan@gmail.com>
2022-08-23 18:11:26 +0800

    Remove unnecessary files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,17 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import ProductsReducer from "../features/products/ProductsSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		products: ProductsReducer,
+	},
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
 >;
