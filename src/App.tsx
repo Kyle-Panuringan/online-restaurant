f3cee1b70df0330d9a4fd8a4c927f514f078472c
Kyle Panuringan <kylepanuringan@gmail.com>
2022-08-23 18:31:30 +0800

    Add app.scss & navbar.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import "./app.scss";
 import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
+			<header>
+				<h1>Online Restaurant</h1>
+				<Navbar />
+			</header>
 			<Hero />
 		</div>
 	);
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="navbar">
+			<ul>
+				<li>Menu</li>
+				<li>About Us</li>
+				<li>Contact Us</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navbar;
