e22febbcb331b429025d321018991aed16a3e500
Kyle Panuringan <kylepanuringan@gmail.com>
2022-08-24 20:59:58 +0800

    Update menu component & style

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,19 @@
+import "./menu.scss";
+
 const Menu = () => {
-	return <div>Menu</div>;
+	return (
+		<div className="menu">
+			<h2>~ Menu ~</h2>
+			<hr id="menu-line" />
+			<div className="menu-buttons">
+				<button>All</button>
+				<button>Burger</button>
+				<button>Pizza</button>
+				<button>Pasta</button>
+				<button>Dessert</button>
+			</div>
+		</div>
+	);
 };
 
 export default Menu;
